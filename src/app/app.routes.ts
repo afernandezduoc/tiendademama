fc6eca0ac86b2a3474f6a1907e7efd47168d3312
Alejandro Fernandez <alej.fernandez@duocuc.cl>
2024-11-16 13:52:00 -0300

    Se crea un menú y se agrega el componente para la gestion de usuarios

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,7 @@ import { RegistroComponent } from './registro/registro.component';
 import { LoginComponent } from './login/login.component';
 import { PerfilComponent } from './perfil/perfil.component';
 import { ListadoProductosComponent } from './listado-productos/listado-productos.component';
+import { GestionUsuariosComponent } from './gestion-usuarios/gestion-usuarios.component';
 
 export const routes: Routes = [
   { path: '', component: InicioComponent },
@@ -13,7 +14,8 @@ export const routes: Routes = [
   { path: 'registro', component: RegistroComponent },
   { path: 'login', component: LoginComponent },
   { path: 'perfil', component: PerfilComponent },
-  { path: 'listado-productos', component: ListadoProductosComponent }
+  { path: 'listado-productos', component: ListadoProductosComponent },
+  { path: 'gestion-usuarios', component: GestionUsuariosComponent }
 ];
 
 export const appRouting = provideRouter(routes);
